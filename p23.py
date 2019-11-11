3b7d2202b686362c8f63e88d313c4a4e76303eda
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 02:33:35 +0600

    Update p23.py

--- p23.py
+++ p23.py
@@ -1,3 +1,9 @@
+# Write a program that reads an integer number and prints its previous and
+# next numbers. See the examples below for the exact format your answers
+# should take. There shouldn't be a space before the period.
+# Remember that you can convert the numbers to strings using the function str.
+# (На английском языке что бы Вы научились понимать The next number for the number 179 is 180. The previous number for the
+# number 179 is 178.)
 a=int(input("Enter your number:"))
 b=a-1
 c=a+1 
